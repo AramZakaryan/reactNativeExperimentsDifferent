ac17b8c9a22c5f0f0c6b33260787983d9731f029
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-03-03 00:50:22 +0400

    Interim Commit

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!
+        lalala
+          uxxx
+
+          rrr
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
